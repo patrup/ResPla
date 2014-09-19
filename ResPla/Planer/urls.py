f49a152697913365cdb7fdca8482b87cd7261f82
Roy Stahn <rs@nowhere.who>
2014-09-19 12:19:57 +0200

    Urls expressions formalized.

--- ResPla/Planer/urls.py
+++ ResPla/Planer/urls.py
@@ -4,8 +4,8 @@ from Planer import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^add_resource/$', views.AddResourceView.as_view(), name='add_resource'),
+    url(r'^resource_add/$', views.AddResourceView.as_view(), name='resource_add'),
     url(r'^resource_list/$', views.ResourceListView.as_view(), name='resource_list'),
-    url(r'^add_person/$', views.AddPersonView.as_view(), name='add_person'),
+    url(r'^person_add/$', views.AddPersonView.as_view(), name='person_add'),
     url(r'^person_list/$', views.PersonListView.as_view(), name='person_list'),
     )
