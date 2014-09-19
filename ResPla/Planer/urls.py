a050b0f5181ce4f10c3494c33f23138a37cff209
Roy Stahn <rs@nowhere.who>
2014-09-19 12:11:47 +0200

    List of all persons implemented. View, url and html-page created.

--- ResPla/Planer/urls.py
+++ ResPla/Planer/urls.py
@@ -6,4 +6,6 @@ urlpatterns = patterns(
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^add_resource/$', views.AddResourceView.as_view(), name='add_resource'),
     url(r'^resource_list/$', views.ResourceListView.as_view(), name='resource_list'),
+    url(r'^add_person/$', views.AddPersonView.as_view(), name='add_person'),
+    url(r'^person_list/$', views.PersonListView.as_view(), name='person_list'),
     )
--- ResPla/Planer/views.py
+++ ResPla/Planer/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView
-from Planer.models import Resource, Booking
+from Planer.models import Resource, Booking, Person
 
 
 class IndexView(ListView):
@@ -22,3 +22,18 @@ class ResourceListView(ListView):
     
     def get_queryset(self):
         return Resource.objects.all()
+    
+    
+class AddPersonView(CreateView):
+    """Adds a new person"""
+    #uses template resource_form.html
+    model = Person
+    fields = ['first_name', 'last_name', 'cost']
+    success_url = '/planer/person_list/'
+    
+
+class PersonListView(ListView):
+    
+    def get_queryset(self):
+        return Person.objects.all()
+    
