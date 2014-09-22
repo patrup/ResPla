0a239b5d5b4d22b8f6a045010e6f2d77cb746c02
Roy Stahn <rs@nowhere.who>
2014-09-22 10:43:47 +0200

    PEP8 coding style in models, urls and views.py implemented.

--- ResPla/Planer/models.py
+++ ResPla/Planer/models.py
@@ -4,20 +4,20 @@ from django.db import models
 class Resource(models.Model):
     title = models.CharField(max_length=100)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
-    
+
     def __str__(self):
         return self.title
 
-    
+
 class Person(models.Model):
     first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)    
+    last_name = models.CharField(max_length=100)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
-    
+
     def __str__(self):
-        return self.first_name + ' ' + self.last_name 
-    
-       
+        return self.first_name + ' ' + self.last_name
+
+
 class Booking(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
@@ -25,6 +25,6 @@ class Booking(models.Model):
     end_date = models.DateTimeField('date finished')
     resource = models.ForeignKey(Resource, null=True)
     person = models.ForeignKey(Person, null=True)
-    
+
     def __str__(self):
         return self.title
--- ResPla/Planer/views.py
+++ ResPla/Planer/views.py
@@ -8,54 +8,53 @@ class IndexView(ListView):
 
     def get_queryset(self):
         return Booking.objects.all()
-    
-    
+
+
 class AddResourceView(CreateView):
     """Adds a new resource"""
-    #uses template resource_form.html
+    # uses template resource_form.html
     model = Resource
     fields = ['title', 'cost']
     success_url = '/planer/resource_list/'
-    
+
 
 class ResourceListView(ListView):
-    
+
     def get_queryset(self):
         return Resource.objects.all()
-    
-    
+
+
 class AddPersonView(CreateView):
     """Adds a new person"""
-    #uses template person_form.html
+    # uses template person_form.html
     model = Person
     fields = ['first_name', 'last_name', 'cost']
     success_url = '/planer/person_list/'
-    
+
 
 class PersonListView(ListView):
-    
+
     def get_queryset(self):
         return Person.objects.all()
-      
+
 
 class AddPersonBookingView(CreateView):
     """Books a person"""
-    #uses template booking_form.html
+    # uses template booking_form.html
     model = Booking
     fields = ['title', 'description', 'start_date', 'end_date', 'person']
     success_url = '/planer/booking_list/'
-    
-    
+
+
 class AddResourceBookingView(CreateView):
     """Books a resource"""
-    #uses template booking_form.html
+    # uses template booking_form.html
     model = Booking
     fields = ['title', 'description', 'start_date', 'end_date', 'resource']
     success_url = '/planer/booking_list/'
-        
+
 
 class BookingListView(ListView):
-    
+
     def get_queryset(self):
         return Booking.objects.all()
-    
