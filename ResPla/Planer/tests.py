846ebe099ab76f6fedad4b9a5c3646d8018cada8
Roy Stahn <rs@nowhere.who>
2014-09-24 15:49:48 +0200

    Test with edge cases for availability of persons implemented. Not complete yet.

--- ResPla/Planer/tests.py
+++ ResPla/Planer/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from datetime import datetime
 
-from Planer.models import Person
+from Planer.models import Person, Booking
 from Planer.views import get_available_persons
 
 
@@ -9,20 +9,39 @@ class ListAvailablePersonsTest(TestCase):
 
     def setUp(self):
         TestCase.setUp(self)
-        Person.objects.create(first_name="Franz",
-                              last_name="Alberts", cost=20.12)
+        franz = Person.objects.create(first_name="Franz",
+                                      last_name="Alberts", cost=20.12)
         Person.objects.create(first_name="Peter",
                               last_name="Phillips", cost=19.12)
-        Person.objects.create(first_name="Availy",
+        Person.objects.create(first_name="Tom",
                               last_name="Nevers", cost=34.07)
+        Booking.objects.create(title="One",
+                               description="The only One.",
+                               start_date=datetime(2014, 10, 2),
+                               end_date=datetime(2014, 10, 5),
+                               person=franz)
 
     def test_db_is_not_empty(self):
         qs = Person.objects.all()
-        self.assertGreater(qs.count(), 0, "DB is empty!")
+        self.assertGreater(qs.count(), 0, "No Persons!")
 
-    def test_three_are_available(self):
+    def test_start_date_before_and_end_date_in_timespan(self):
         start_date = datetime(2014, 10, 1)
         end_date = datetime(2014, 10, 3)
-        qs = get_available_persons(start_date, end_date)
-        count = qs.count()
-        self.assertEqual(count, 3, "Should be three entries!")
+        available_persons = get_available_persons(start_date, end_date)
+        franz_exists = available_persons.filter(first_name="Franz").exists()
+        self.assertFalse(franz_exists, "Franz should not be available!")
+
+    def test_start_date_in_and_end_date_after_timespan(self):
+        start_date = datetime(2014, 10, 3)
+        end_date = datetime(2014, 10, 10)
+        available_persons = get_available_persons(start_date, end_date)
+        franz_exists = available_persons.filter(first_name="Franz").exists()
+        self.assertFalse(franz_exists, "Franz should not be available!")
+
+    def test_start_date_in_and_end_date_in_timespan(self):
+        start_date = datetime(2014, 10, 3)
+        end_date = datetime(2014, 10, 4)
+        available_persons = get_available_persons(start_date, end_date)
+        franz_exists = available_persons.filter(first_name="Franz").exists()
+        self.assertFalse(franz_exists, "Franz should not be available!")
