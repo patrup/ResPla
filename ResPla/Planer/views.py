eac0ca634286860967c3f44ef5eb1364b049659d
Roy Stahn <rs@nowhere.who>
2014-10-02 10:41:24 +0200

    Booking of an available resource implemented. Works like person booking. Adding of comments.

--- ResPla/Planer/views.py
+++ ResPla/Planer/views.py
@@ -24,6 +24,21 @@ def get_available_persons_inside_span(start_date, end_date):
     return Person.objects.all().exclude(q1 & q2)
 
 
+def get_available_resources(start_date, end_date):
+    q1 = Q(booking__start_date__range=(start_date, end_date))
+    q2 = Q(booking__end_date__range=(start_date, end_date))
+    qset = get_available_resources_inside_span(start_date, end_date)
+    return qset.all().exclude(q1 | q2)
+
+
+def get_available_resources_inside_span(start_date, end_date):
+    '''Gets all available resources with start_date and end_date
+    in booking time span'''
+    q1 = Q(booking__start_date__lt=start_date)
+    q2 = Q(booking__end_date__gt=end_date)
+    return Resource.objects.all().exclude(q1 & q2)
+
+
 class IndexView(ListView):
     template_name = 'Planer/index.html'
 
@@ -119,21 +134,29 @@ class CreateTimeSpanView(ListView):
         return get_available_persons(sd, ed)
 
 
-class TimeSpanForm(forms.Form):
+class PersonsTimeSpanForm(forms.Form):
     start_date = forms.DateTimeField()
     end_date = forms.DateTimeField()
     persons = forms.ModelChoiceField(queryset=Person.objects.all(),
                                      required=False)
 
 
+class ResourceTimeSpanForm(forms.Form):
+    start_date = forms.DateTimeField()
+    end_date = forms.DateTimeField()
+    resources = forms.ModelChoiceField(queryset=Resource.objects.all(),
+                                       required=False)
+
+
 def book_a_person(request):
+    ''' books an available person '''
     if request.method == 'GET':
         person_list = Person.objects.all()
-        form = TimeSpanForm()
+        form = PersonsTimeSpanForm()
         context = {'person_list': person_list, 'form': form}
         return render(request, 'Planer/book_person.html', context)
     if request.method == 'POST':
-        form = TimeSpanForm(request.POST)
+        form = PersonsTimeSpanForm(request.POST)
         if form.is_valid():
             if form.cleaned_data['persons'] is not None:
                 person = form.cleaned_data['persons']
@@ -154,3 +177,34 @@ def book_a_person(request):
             person_list = Person.objects.all()
         context = {'person_list': person_list, 'form': form}
         return render(request, 'Planer/book_person.html', context)
+
+
+def book_a_resource(request):
+    ''' books an available resource '''
+    if request.method == 'GET':
+        resource_list = Resource.objects.all()
+        form = ResourceTimeSpanForm()
+        context = {'resource_list': resource_list, 'form': form}
+        return render(request, 'Planer/book_resource.html', context)
+    if request.method == 'POST':
+        form = ResourceTimeSpanForm(request.POST)
+        if form.is_valid():
+            if form.cleaned_data['resources'] is not None:
+                resource = form.cleaned_data['resources']
+                booking = Booking()
+                booking.resource = resource
+                booking.start_date = form.cleaned_data['start_date']
+                booking.end_date = form.cleaned_data['end_date']
+                booking.title = resource.title
+                booking.description = "Resourcenbuchung"
+                booking.save()
+                return render(request, 'Planer/index.html')
+            else:
+                start_date = form.cleaned_data['start_date']
+                end_date = form.cleaned_data['end_date']
+                resource_list = get_available_resources(start_date, end_date)
+                form.fields['resources'].queryset = resource_list
+        else:
+            resource_list = Resource.objects.all()
+        context = {'resource_list': resource_list, 'form': form}
+        return render(request, 'Planer/book_resource.html', context)
